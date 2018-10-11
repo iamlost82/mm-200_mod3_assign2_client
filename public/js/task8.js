402b590e460de3464b0d6ec00514ca27332f0608
Otto <otto.andre@deep-north.com>
2018-10-11 16:46:14 +0200

    Task 8 complete

--- public/js/task8.js
+++ public/js/task8.js
@@ -3,5 +3,5 @@ let myChart = new FancyChart(cnv, [20, 45, 65, 90]);
 myChart.drawBarChart();
 
 let cnv2 = document.getElementById("myCanvas2");
-let myChart2 = new FancyChart(cnv2, [10,10,10,10,20,10,30,10,60]);
+let myChart2 = new FancyChart(cnv2, [20, 45, 65, 90]);
 myChart2.drawPieChart();
